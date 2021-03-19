fbadce19a5167f393a9cce3f3de9f48015956c38
JocoAus <joachim@codenation.com>
2021-03-19 22:50:42 +1100

    Update [article].js

--- pages/news/[article].js
+++ pages/news/[article].js
@@ -48,14 +48,14 @@ export default function Article(props) {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1"
+          src="https://www.youtube.com/embed/iZSBPMJP_00?&autoplay=1"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>{' '}
         <br />
         <h1>
-          Nah gammon BRUZ. Shame Job I bet you wanna get your friends now, too.{` `}
+          Shame Job I bet you wanna get your friends now, too.{` `}
           <Link href="/">
             <a>Go ahead, do it.</a>
           </Link>
